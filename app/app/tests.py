91ff03f59b1a09aef8b055ce273336e73dc786af
Hristo Georgiev <hristo.i.georgiev@gmail.com>
2024-03-12 08:53:58 +0200

    fix linting

--- app/app/tests.py
+++ app/app/tests.py
@@ -3,6 +3,7 @@ Sample tests
 """
 from django.test import SimpleTestCase
 
+
 from app import calc
 
 
@@ -16,8 +17,7 @@ class CalcTests(SimpleTestCase):
         self.assertEqual(res, 11)
 
     def test_subtract_numbers(self):
-        """Test subtracting numbers""" 
+        """Test subtracting numbers"""
         res = calc.subtract(10, 15)
-        
+
         self.assertEqual(res, 5)
-    
