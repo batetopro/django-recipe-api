796c03cdc89351d815d2342a608e1934bb5a2b88
Hristo Georgiev <hristo.i.georgiev@gmail.com>
2024-03-14 15:23:21 +0200

    tags - list, update, delete

--- app/core/models.py
+++ app/core/models.py
@@ -56,6 +56,8 @@ class Recipe(models.Model):
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE
     )
+    tags = models.ManyToManyField('Tag')
+
     title = models.CharField(max_length=255)
     description = models.TextField(blank=True)
     time_minutes = models.IntegerField()
@@ -64,3 +66,15 @@ class Recipe(models.Model):
 
     def __str__(self) -> str:
         return self.title
+
+
+class Tag(models.Model):
+    """Tag for filtering recipes."""
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+    name = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return self.name
