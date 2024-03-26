736ed45c87a0991f4266203429b50091eef0f2ad
Hristo Georgiev <hristo.i.georgiev@gmail.com>
2024-03-26 13:01:25 +0200

    Link / unlink ingredients witth a recipe

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -27,10 +27,13 @@ class RecipeSerializer(serializers.ModelSerializer):
     """Serializer for recipes."""
 
     tags = TagSerializer(many=True, required=False)
+    ingredients = TagSerializer(many=True, required=False)
 
     class Meta:
         model = Recipe
-        fields = ['id', 'title', 'time_minutes', 'price', 'link', 'tags']
+        fields = ['id', 'title', 'time_minutes', 'price', 'link',
+                  'tags', 'ingredients'
+                  ]
         read_only_fields = ['id']
 
     def _link_tags(self, instance, tags):
@@ -43,19 +46,37 @@ class RecipeSerializer(serializers.ModelSerializer):
             )
             instance.tags.add(tag_obj)
 
+    def _link_ingredients(self, instance, ingredients):
+        """Handle getting or creating ingredients as needed."""
+        auth_user = self.context['request'].user
+        for ingredient in ingredients:
+            ingredient_obj, created = Ingredient.objects.get_or_create(
+                user=auth_user,
+                **ingredient
+            )
+            instance.ingredients.add(ingredient_obj)
+
     def create(self, validated_data):
         """Create a recipe."""
         tags = validated_data.pop('tags', [])
+        ingredients = validated_data.pop('ingredients', [])
         recipe = Recipe.objects.create(**validated_data)
         self._link_tags(recipe, tags)
+        self._link_ingredients(recipe, ingredients)
         return recipe
 
     def update(self, instance, validated_data):
         """Update a recipe."""
         tags = validated_data.pop('tags', None)
+        ingredients = validated_data.pop('ingredients', None)
+
         if tags is not None:
             instance.tags.clear()
             self._link_tags(instance, tags)
+
+        if ingredients is not None:
+            instance.ingredients.clear()
+            self._link_ingredients(instance, ingredients)
 
         for attr, value in validated_data.items():
             setattr(instance, attr, value)
